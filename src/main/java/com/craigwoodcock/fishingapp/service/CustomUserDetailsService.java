60aab6b23c2ba1956f17d22ff1dd8d3ba2bc6aca
Craig Woodcock <cwoodcock@spartaglobal.com>
2024-07-23 23:43:00 +0100

    added customuserdetails

---
+++ src/main/java/com/craigwoodcock/fishingapp/service/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.craigwoodcock.fishingapp.service;
+
+import com.craigwoodcock.fishingapp.model.User;
+import com.craigwoodcock.fishingapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+   @Autowired
+    private UserRepository userRepository;
+
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+       User user = userRepository.findByUsername(username).orElseThrow(()-> new UsernameNotFoundException("User not found"+username));
+
+       return new org.springframework.core.userdetails.User(
+               user.getUsername(),
+               user.getPassword(),
+               user.getAuthorities()
+       );
+    }
+}
--- src/main/java/com/craigwoodcock/fishingapp/service/UserService.java
+++ src/main/java/com/craigwoodcock/fishingapp/service/UserService.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
-    private UserRepository userRepository;
-    private PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
     public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
