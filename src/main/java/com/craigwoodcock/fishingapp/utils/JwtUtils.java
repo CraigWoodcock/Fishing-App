1ec293759c5c7af155ffb8890717bd38bdb3bbd3
Craig Woodcock <cwoodcock@spartaglobal.com>
2024-10-30 20:56:03 +0000

    adding helper methods and removed deprecated methods from JwtUtils

--- src/main/java/com/craigwoodcock/fishingapp/utils/JwtUtils.java
+++ src/main/java/com/craigwoodcock/fishingapp/utils/JwtUtils.java
@@ -1,36 +1,55 @@
 package com.craigwoodcock.fishingapp.utils;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
-import java.security.Key;
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Component
 public class JwtUtils {
     private static final long EXPIRATION_TIME = 864_000_000; // 10 days
-    private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private static final SecretKey SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
 
     public String generateToken(String username) {
+
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + EXPIRATION_TIME);
+
         return Jwts.builder()
                 .subject(username)
                 .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis()+EXPIRATION_TIME))
-                .signWith(key)
+                .expiration(expiryDate)
+                .signWith(SECRET_KEY)
                 .compact();
     }
 
     public String getUsernameFromToken(String token) {
-        Claims claims = Jwts.parser()
-                .setSigningKey(key)
-                .build()
-                .parseSignedClaims(token)
-                .getPayload();
+        Claims claims = extractAllClaims(token);
+
         return claims.getSubject();
     }
 
-    
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(SECRET_KEY)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public boolean validateToken(String token) {
+        try{
+            extractAllClaims(token);
+            return true;
+        }catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
 }
