3ade4a1ba3db3bade94909e235c925fa1ab0d08d
Craig Woodcock <cwoodcock@spartaglobal.com>
2024-11-07 00:31:52 +0000

    added register method to api controller

--- src/main/java/com/craigwoodcock/fishingapp/apiController/AuthApiController.java
+++ src/main/java/com/craigwoodcock/fishingapp/apiController/AuthApiController.java
@@ -1,12 +1,12 @@
 package com.craigwoodcock.fishingapp.apiController;
 
+import com.craigwoodcock.fishingapp.exception.UserAlreadyExistsException;
 import com.craigwoodcock.fishingapp.model.AuthRequest;
 import com.craigwoodcock.fishingapp.model.AuthResponse;
 import com.craigwoodcock.fishingapp.model.User;
 import com.craigwoodcock.fishingapp.service.UserService;
 import com.craigwoodcock.fishingapp.utils.JwtUtils;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,6 +35,18 @@ public class AuthApiController {
 
             return ResponseEntity.ok(new AuthResponse(token, user.getUsername()));
         }
-        throw new BadCredentialsException("Invalid username or password");
+        return ResponseEntity.badRequest().body(new AuthResponse("Invalid username or password", user.getUsername()));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody User user) {
+        try {
+            userService.registerUser(user);
+            return ResponseEntity.ok(new AuthResponse(jwtUtils.generateToken(user.getUsername()), user.getUsername()));
+        } catch (UserAlreadyExistsException e) {
+            return ResponseEntity.badRequest().body(new AuthResponse("User already exists", user.getUsername()));
+//            throw new UserAlreadyExistsException("User already exists");
+        }
+
     }
 }
