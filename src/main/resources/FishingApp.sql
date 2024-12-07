0c2c3895ea2d76ded43f4ecfe7d3e54ab22ba751
Craig Woodcock <cwoodcock@spartaglobal.com>
2024-12-07 08:08:45 +0000

    changing sql filename

--- src/main/resources/FishingApp.sql
+++ src/main/resources/FishingApp.sql
@@ -1,38 +1,49 @@
+drop database if exists fishing_app;
+
 CREATE database if not exists fishing_app;
+
 USE fishing_app;
+
 drop table if exists users;
+
 create table if not exists users
 (
-    id       bigint primary key auto_increment not NULL,
-    username varchar(50) unique                not null,
-    name     varchar(50)                       not null,
-    password varchar(500)                      not null,
-    email    varchar(100) unique               not null,
-    role     varchar(20)                       not null
+    id         bigint primary key auto_increment not NULL,
+    username   varchar(50) unique                not null,
+    name       varchar(50)                       not null,
+    password   varchar(500)                      not null,
+    email      varchar(100) unique               not null,
+    role       varchar(20)                       not null,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) auto_increment = 31982;
 
-select *
-from users;
+drop table if exists sessions;
 
-CREATE TABLE sessions
+CREATE TABLE if not exists sessions
 (
     id             BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     user_id        BIGINT                            NOT NULL,
     venue          VARCHAR(100)                      NOT NULL,
     start_date     DATETIME                          NOT NULL,
     duration_hours INT                               NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id)
+    created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    foreign key (user_id) references users (id)
 );
 
-CREATE TABLE anglers
+drop table if exists anglers;
+CREATE TABLE if not exists anglers
 (
     id         BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    session_id BIGINT                            NOT NULL,
     name       VARCHAR(50)                       NOT NULL,
-    FOREIGN KEY (session_id) REFERENCES sessions (id)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE catches
+drop table if exists catches;
+
+CREATE TABLE if not exists catches
 (
     id          BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     angler_id   BIGINT                            NOT NULL,
@@ -46,3 +57,30 @@ CREATE TABLE catches
     FOREIGN KEY (angler_id) REFERENCES anglers (id),
     FOREIGN KEY (session_id) REFERENCES sessions (id)
 );
+
+CREATE TABLE angler_session
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    angler_id  BIGINT                            NOT NULL,
+    session_id BIGINT                            NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (angler_id) REFERENCES anglers (id),
+    FOREIGN KEY (session_id) REFERENCES sessions (id)
+);
+
+CREATE TABLE IF NOT EXISTS jwt_tokens
+(
+    id          BIGINT PRIMARY KEY AUTO_INCREMENT,
+    token       VARCHAR(255) NOT NULL,
+    user_id     BIGINT       NOT NULL,
+    expiry_date TIMESTAMP    NOT NULL,
+    revoked     BOOLEAN      NOT NULL DEFAULT FALSE,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+-- INSERT INTO users (username, name, password, email, role)
+-- VALUES ('craig', 'craig woodcock', 'craig', 'craig@example.com', 'USER');
+-- drop table angler_session; 
+-- drop table sessions;
+-- drop table catches;
+
