36ec4781c9bc701a98ae8ec15ca7e131eead1547
Craig Woodcock <cwoodcock@spartaglobal.com>
2025-02-02 12:07:39 +0000

    renaming variable in gitlab-yml file, incorrect spelling

--- src/main/java/com/craigwoodcock/fishingapp/FishingAppApplication.java
+++ src/main/java/com/craigwoodcock/fishingapp/FishingAppApplication.java
@@ -3,11 +3,8 @@ package com.craigwoodcock.fishingapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.logging.Logger;
-
 @SpringBootApplication
 public class FishingAppApplication {
-    private static final Logger log = Logger.getLogger(FishingAppApplication.class.getName());
 
     public static void main(String[] args) {
         SpringApplication.run(FishingAppApplication.class, args);
